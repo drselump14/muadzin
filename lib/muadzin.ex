0cf53536968cd1bad59248e421237fde16b8ebc7
slamet kristanto <botoksgonzales@gmail.com>
2022-11-21 08:05:19 +0900

    First commmit

---
+++ lib/muadzin.ex
@@ -0,0 +1,18 @@
+defmodule Muadzin do
+  @moduledoc """
+  Documentation for Muadzin.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Muadzin.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/muadzin_test.exs
@@ -0,0 +1,8 @@
+defmodule MuadzinTest do
+  use ExUnit.Case
+  doctest Muadzin
+
+  test "greets the world" do
+    assert Muadzin.hello() == :world
+  end
+end
